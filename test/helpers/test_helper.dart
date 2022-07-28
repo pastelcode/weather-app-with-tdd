109b849aa30c11492b6d45adc00bd774df87d75a
Pastel <pastel.dev.gt@gmail.com>
2022-07-28 17:12:57 -0600

    Add mock `RemoteDataSource`

--- test/helpers/test_helper.dart
+++ test/helpers/test_helper.dart
@@ -1,10 +1,12 @@
+import 'package:flutter_weather_app_with_tdd/data/data.dart';
 import 'package:mockito/annotations.dart';
 import 'package:http/http.dart' as http;
-import 'package:flutter_weather_app_with_tdd/domain/repositories/repositories.dart';
+import 'package:flutter_weather_app_with_tdd/domain/domain.dart';
 
 @GenerateMocks(
   [
     WeatherRepository,
+    RemoteDataSource,
   ],
   customMocks: [MockSpec<http.Client>(as: #MockHttpClient)],
 )
