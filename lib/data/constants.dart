47efef1c426740a45883435db6db2867e579f9ca
Pastel <pastel.dev.gt@gmail.com>
2022-07-28 13:04:07 -0600

    Add dot env package for api key

---
+++ lib/data/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Urls {
+  static const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  static final apiKey = dotenv.env['OPEN_WEATHER_API_KEY'] ?? '';
+  static String currentWeatherByName(String city) =>
+      '$baseUrl?q=$city&appid=$apiKey';
+  static String weatherIcon(String iconCode) =>
+      'https://openweathermap.org/img/wn/$iconCode@2x.png';
+}
