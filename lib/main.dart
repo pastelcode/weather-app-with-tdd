abb45001723393ff2e455b3a84e92d4082260f34
Pastel <pastel.dev.gt@gmail.com>
2022-07-29 11:17:13 -0600

    Customize app theme

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,13 @@ class MyApp extends StatelessWidget {
         title: 'Weather app',
         theme: ThemeData(
           primarySwatch: Colors.orange,
+          appBarTheme: const AppBarTheme(
+            elevation: 0,
+            toolbarHeight: 100,
+            backgroundColor: Colors.white,
+            centerTitle: true,
+          ),
+          scaffoldBackgroundColor: Colors.white,
         ),
         home: const WeatherPage(),
       ),
