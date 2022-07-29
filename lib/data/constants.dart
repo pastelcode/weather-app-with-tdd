2f32f1f545cb00d9ab37a93e320c0a39a9755130
Pastel <pastel.dev.gt@gmail.com>
2022-07-28 18:44:28 -0600

    Read api key from env file

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -4,7 +4,7 @@ class Urls {
   static const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
   static final apiKey = dotenv.env['OPEN_WEATHER_API_KEY'] ?? '';
   static String currentWeatherByName(String city) =>
-      '$baseUrl?q=$city&appid=$apiKey';
+      '$baseUrl?q=$city&units=metric&appid=$apiKey';
   static String weatherIcon(String iconCode) =>
       'https://openweathermap.org/img/wn/$iconCode@2x.png';
 }
